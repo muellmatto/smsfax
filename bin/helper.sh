2ba5ebee42c2872c0ff3e8e8acbcad9b49725884
matto <matto@damniam.de>
2015-12-16 16:23:24 +0100

    print status on daemon start

--- bin/helper.sh
+++ bin/helper.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 /usr/bin/sleep 30
+
+/usr/bin/gammu getdatetime && /usr/bin/gammu getdatetime | /home/matto/Workspace/engelsmsfax/smsfax/bin/thermo.sh || echo "Das Modem deines Engel-SMS-Fax funktioniert nicht richtig :(" | /home/matto/Workspace/engelsmsfax/smsfax/bin/thermo.sh
+
+/usr/bin/sleep 5
+
 /usr/bin/gammu-smsd --pid=/var/run/gammu-smsd.pid --daemon
 
