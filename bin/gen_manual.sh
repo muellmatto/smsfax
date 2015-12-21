f53e0fe9c2304262d61bee1720f6af12dd0ea254
matto <matto@damniam.de>
2015-12-21 15:24:37 +0100

    typo

--- bin/gen_manual.sh
+++ bin/gen_manual.sh
@@ -7,17 +7,28 @@ bindir="/home/matto/Workspace/engelsmsfax/smsfax/bin"
 source $bindir/sms.sh
 
 
-echo "Das Engelsmsfax bietet neben der schlichten Textausgabe auch ein paar Darstellungs-Features! So geht es:
-Vor den SMS-TEXT 4 Zeichen setzen. Das erste ist immer '#'.
-Mit dem zweiten Zeichen wird die die Aktion gewählt: 
+echo "Das Engel-Sms-Fax
+bietet neben der
+schlichten Textausgabe
+auch ein paar
+Darstellungs-Features!
+So geht es:
+Vor den SMS-TEXT 4
+Zeichen setzen. Das
+erste ist immer '#'.
+Mit dem zweiten Zeichen
+wird die Aktion gewählt: 
 'A' = AsciiArt
 'C' = Cowsay,
 'F' = Font.
-Das dritte Zeichen wählt den 'Style' aus. Abschließend kommt ein Leerzeichen.
-
-Soll Stimpy in einer Sprechblase 'Oi!' rufen, würde die sms lauten: 
+Das dritte Zeichen
+wählt den 'Style' aus.
+Abschließend kommt ein
+Leerzeichen.
+Soll Stimpy in einer
+Sprechblase Oi! rufen,
+würde die sms lauten:
 '#CS Oi!'
-
 Alle Möglichkeiten sind folgend dargestellt:
 " | $bindir/thermo.sh
 
@@ -27,7 +38,7 @@ echo -e "\n\n" | $bindir/thermo.sh
 for m in $(cut -d ';' -f 1 $bindir/art.csv) ; do  intro="#A$m" ; m="#A$m "; parser | $bindir/thermo.sh ; done
 
 ## cowsay
-for m in $(cut -d ';' -f 1 $bindir/cowsay.csv) ; do intro="$m" ; m="#C$m Oi!" ; parser | $bindir/thermo.sh ; done 
+for m in $(cut -d ';' -f 1 $bindir/cowsay.csv) ; do intro="#C$m" ; m="#C$m Oi!" ; parser | $bindir/thermo.sh ; done 
 
 ## figlet fonts
 for m in $(cut -d ';' -f 1 $bindir/fonts.csv) ; do intro="#F$m" ; m="#F$m Oi!" ; parser | $bindir/thermo.sh ; done
