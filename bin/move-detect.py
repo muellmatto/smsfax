cb99e34f1ca4db255aef52f9488d819018f878a6
matto <matto@damniam.de>
2016-02-07 00:25:21 +0100

    Threading für das Augenleuchten während der Sprachausgabe

--- bin/move-detect.py
+++ bin/move-detect.py
@@ -6,13 +6,12 @@ import signal
 import os
 import sys
 import random
-
+import threading
 
 SAMPLES_PATH="/home/alarm/samples/"
 TEXTE_FILE = "/home/alarm/texte.txt"
 
 SENSOR_PIN = 4
-# LED_PIN = 23
 LED_PIN_A = 17
 LED_PIN_B = 27
 
@@ -28,7 +27,6 @@ WAVE_FILES=os.listdir(SAMPLES_PATH)
 ## Setup GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SENSOR_PIN, GPIO.IN)
-# GPIO.setup(LED_PIN, GPIO.OUT) # Gelb  
 GPIO.setup(LED_PIN_A, GPIO.OUT) # Rot
 GPIO.setup(LED_PIN_B, GPIO.OUT) # Rot  
 
@@ -41,19 +39,26 @@ auge2=GPIO.PWM(LED_PIN_B,100)
 auge1.start(0) 
 auge2.start(0)
 
-pause_time = 0.02
+pause_time = 0.01
 
 
-
+## PWM Funktion
+def blink():
+    while LEUCHTE==1:  
+        for i in range(0,101): 
+            auge1.ChangeDutyCycle(i)  
+            auge2.ChangeDutyCycle(i)  
+            time.sleep(pause_time)  
+        for i in range(100,-1,-1): 
+            auge1.ChangeDutyCycle(i)  
+            auge2.ChangeDutyCycle(i) 
+            time.sleep(pause_time)  
 
 ## Sigterm abfangen und LEDs wieder aus machen!!
 def signal_term_handler(signal, frame):
     GPIO.remove_event_detect(SENSOR_PIN)
-    # GPIO.output(LEN_PIN,False)
     auge1.stop()
     auge2.stop() 
-    # GPIO.output(LEN_PIN_A,False) 
-    # GPIO.output(LEN_PIN_B,False) 
     GPIO.cleanup()
     sys.exit(0)
 
@@ -61,17 +66,15 @@ signal.signal(signal.SIGTERM, signal_term_handler)
 
 ## Sprachausgabe
 def sprich(bla):
-    GPIO.output(LED_PIN_A, True)
-    GPIO.output(LED_PIN_B, True)
     GPIO.remove_event_detect(SENSOR_PIN)
-    # GPIO.output(LED_PIN, True)
+    ## Thread
+    LEUCHTE=1
+    augen=threading.Thread(target=blink)
+    augen.start()
     WAVE_FILE=random.choice( WAVE_FILES )
     os.system("/usr/bin/aplay " + SAMPLES_PATH + WAVE_FILE)
-    GPIO.output(LED_PIN_A, False)
-    GPIO.output(LED_PIN_B, False)
+    LEUCHTE=0
     time.sleep( random.randrange(SLEEP_MIN, SLEEP_MAX, SLEEP_STEP) )
-    # time.sleep(3)
-    # GPIO.output(LED_PIN, False)
     GPIO.add_event_detect(SENSOR_PIN, GPIO.RISING, callback=sprich, bouncetime=300)
 
 
@@ -97,13 +100,3 @@ except:
 
 
 
-#### UFF
-#    while True:  
-#        for i in range(0,101): 
-#            auge1.ChangeDutyCycle(i)  
-#            auge2.ChangeDutyCycle(i)  
-#            time.sleep(pause_time)  
-#        for i in range(100,-1,-1): 
-#            auge1.ChangeDutyCycle(i)  
-#            auge2.ChangeDutyCycle(i) 
-#            time.sleep(pause_time)  
